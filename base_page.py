6aa739aa60eb2666da3df8b98822dc416542518f
unknown <yukka83@gmail.com>
2023-04-11 19:20:28 +0200

    Initial commit

---
+++ base_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+		
+	def open(self): 
+	    link = "http://selenium1py.pythonanywhere.com/"
+	    self.browser.get(link)
+	    open(browser)
