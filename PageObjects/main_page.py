daf45a5fa062b06674d7df5075095dd08b3162fb
unknown <yukka83@gmail.com>
2023-04-15 00:23:29 +0200

    add new file and rename folder

--- PageObjects/main_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from .base_page import BasePage
-
-
-class MainPage(BasePage):
-
-    def go_to_login_page(self):
-	    self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-		login_link.click()
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
+
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-		
-	def open(self): 
-	    link = "http://selenium1py.pythonanywhere.com/"
-	    self.browser.get(link)
-	    open(browser)
+
+    def open(self):
+        link = "http://selenium1py.pythonanywhere.com/"
+        self.browser.get(link)
+	      
