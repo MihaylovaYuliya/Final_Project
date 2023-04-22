174b3e61014c8e161c67781318d644f5927a7088
unknown <yukka83@gmail.com>
2023-04-22 15:38:22 +0200

    add new file

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+		login_url = self.browser.current_url
+        assert "some_word" in self.browser.current_url, "'login' not in current url" 
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.login_form), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.registration_form), "Registration form is not presented"
