da0d1163abb913b6e0797c63e4daf69f63b3380f
unknown <yukka83@gmail.com>
2023-04-24 17:40:19 +0200

    add new raws

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.login_link)
         login_link.click()
+		
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.login_link), "Login link is not presented"
