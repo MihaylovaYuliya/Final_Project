a33be359a8aeea43934d8b914d06dddd52de1864
unknown <yukka83@gmail.com>
2023-04-24 17:56:18 +0200

    add new  method

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,7 +8,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.login_link)
         login_link.click()
-		
+		alert = self.browser.switch_to.alert
+		alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.login_link), "Login link is not presented"
